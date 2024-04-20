129036676199e905bb31bd5f2e67daa638a5908b
Xavier Pessoles <xpessoles.ptsi@free.fr>
2024-04-20 08:59:11 +0200

    Update make_page_systemes.py

---
+++ docs/Labo_SII/make_page_systemes.py
@@ -0,0 +1,204 @@
+
+liste_dico_syst = [
+    {
+    'num'           :'01',
+    'systeme'       :'bgr-300',
+    'nom'           :'BGR-300',
+    'equip'         :'DIDASTEL',
+    'site_equip'    :'http://www.didastel.fr/',
+    'description'   :"Boule gyrostabilisée 2 étages",
+    'logo'          :"../img/bgr-300.png",
+    'page'          :"bgr-300"
+    },
+    {
+    'num'           :'02',
+    'systeme'       :'bras-beta',
+    'nom'           :'Bras BETA',
+    'equip'         :'S2IDIDAC',
+    'site_equip'    :'https://s2ididac.com/',
+    'description'   :"Le Bras BETA reproduit un système de cotrôle de tubes de géérateur de vapeur.",
+    'logo'          :"../img/bras-beta.png",
+    'page'          :"bras-beta"
+    },
+    {
+    'num'           :'03',
+    'systeme'       :'cheville-nao',
+    'nom'           :'Cheville NAO',
+    'equip'         :'ERM',
+    'site_equip'    :'',
+    'description'   :"Cheville didactisée du robot NAO",
+    'logo'          :"../img/cheville-nao.png",
+    'page'          :"cheville-nao"
+    },
+    {
+    'num'           :'04',
+    'systeme'       :'comax',
+    'nom'           :'CoMAX',
+    'equip'         :'DIDASTEL',
+    'site_equip'    :'',
+    'description'   :"Le robot collaboratif CoMAX est un système permettant d'assister le maniemnent des charges lourdes.",
+    'logo'          :"../img/comax.png",
+    'page'          :"comax"
+    },
+    {
+    'num'           :'05',
+    'systeme'       :'controlx',
+    'nom'           :'ControlX',
+    'equip'         :'DMS',
+    'site_equip'    :'',
+    'description'   :"Le controle X est un axe industriel asservi.",
+    'logo'          :"../img/controlx.png",
+    'page'          :"controlx"
+    },
+    {
+    'num'           :'06',
+    'systeme'       :'cordeuse',
+    'nom'           :'Cordeuse',
+    'equip'         :'DMS',
+    'site_equip'    :'',
+    'description'   :"Cordeuse de raquette didactisée.",
+    'logo'          :"../img/cordeuse.png",
+    'page'          :"cordeuse"
+    },
+    {
+    'num'           :'07',
+    'systeme'       :'dae',
+    'nom'           :'Direction Assistée Electrique',
+    'equip'         :'DMS',
+    'site_equip'    :'',
+    'description'   :"La DAE est un système instrumentée issue d'une TWINGO",
+    'logo'          :"../img/dae.png",
+    'page'          :"dae"
+    },
+    {
+    'num'           :'08',
+    'systeme'       :'d2c',
+    'nom'           :'D2C',
+    'equip'         :'DMS',
+    'site_equip'    :'',
+    'description'   :"Le Drone Didactique Commandé permet d'anlyser le tangage d'un drone",
+    'logo'          :"../img/d2c.png",
+    'page'          :"d2c"
+    },
+    {
+    'num'           :'09',
+    'systeme'       :'ericc',
+    'nom'           :'Ericc 3',
+    'equip'         :'',
+    'site_equip'    :'',
+    'description'   :"Le robot Ericc 3 est un robot industriel 6 axes.",
+    'logo'          :"../img/ericc.png",
+    'page'          :"ericc"
+    },
+    {
+    'num'           :'10',
+    'systeme'       :'evolap',
+    'nom'           :'Evolap',
+    'equip'         :'DMS',
+    'site_equip'    :'',
+    'description'   :"L'evolap s'inspire du fonctionnement d'un robot endoscopique.",
+    'logo'          :"../img/evolap.png",
+    'page'          :"evolap"
+    },
+    {
+    'num'           :'11',
+    'systeme'       :'maxpid',
+    'nom'           :'MaxPID',
+    'equip'         :'DIDASTEL',
+    'site_equip'    :'DIDASTEL',
+    'description'   :"Les MaxPID et MaxPID-E sont la partie opérative de robots.",
+    'logo'          :"../img/maxpid.png",
+    'page'          :"maxpid"
+    },
+    {
+    'num'           :'12',
+    'systeme'       :'moteurcc',
+    'nom'           :'Moteur à courant continu',
+    'equip'         :'3sigma',
+    'site_equip'    :'',
+    'description'   :"Platine équipée d'un moteur à courant continu permettant d'analyser son fonctionnement.",
+    'logo'          :"../img/moteurcc.png",
+    'page'          :"moteurcc"
+    },
+    {
+    'num'           :'13',
+    'systeme'       :'moby-crea',
+    'nom'           :'Moby Crea',
+    'equip'         :'CREA',
+    'site_equip'    :'',
+    'description'   :"Système permettant d'aider au bercement des bébés.",
+    'logo'          :"../img/moby-crea.png",
+    'page'          :"moby-crea"
+    },
+    {
+    'num'           :'14',
+    'systeme'       :'pilote-auto',
+    'nom'           :'Pilote Automatique de Voilier',
+    'equip'         :'CREA',
+    'site_equip'    :'',
+    'description'   :"Bloc hydraulique permettant d'asservir la position du safran d'un voilier.",
+    'logo'          :"../img/pilote-auto.png",
+    'page'          :"pilote-auto"
+    },
+    {
+    'num'           :'15',
+    'systeme'       :'portail',
+    'nom'           :'Portail ABB',
+    'equip'         :'',
+    'site_equip'    :'',
+    'description'   :"Fermeture automatique d'un portail.",
+    'logo'          :"../img/portail.png",
+    'page'          :"portail"
+    },
+    {
+    'num'           :'16',
+    'systeme'       :'rc4',
+    'nom'           :'Robot à câbles RC4',
+    'equip'         :'DIDASTEL',
+    'site_equip'    :'',
+    'description'   :"Robot à câbles plan",
+    'logo'          :"../img/rc4.png",
+    'page'          :"rc4"
+    },
+    {
+    'num'           :'17',
+    'systeme'       :'robot-delta',
+    'nom'           :'Robot Delta 2D',
+    'equip'         :'3sigma',
+    'site_equip'    :'',
+    'description'   :"Robot delta2D plan",
+    'logo'          :"../img/robot-delta.png",
+    'page'          :"robot-delta"
+    },
+    {
+    'num'           :'18',
+    'systeme'       :'robot-haptique',
+    'nom'           :'Robot Haptique',
+    'equip'         :'',
+    'site_equip'    :'',
+    'description'   :"Robot à retour de forces.",
+    'logo'          :"../img/robot-haptique.png",
+    'page'          :"robot-haptique"
+    },
+    {
+    'num'           :'1',
+    'systeme'       :'',
+    'nom'           :'',
+    'equip'         :'',
+    'site_equip'    :'',
+    'description'   :"",
+    'logo'          :"../img/.png",
+    'page'          :""
+    },
+    {
+    'num'           :'2',
+    'systeme'       :'',
+    'nom'           :'',
+    'equip'         :'',
+    'site_equip'    :'',
+    'description'   :"",
+    'logo'          :"../img/.png",
+    'page'          :""
+    },
+
+]
